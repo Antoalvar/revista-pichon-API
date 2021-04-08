981f4b7f1c5fc392a28050815265983542a7a627
Antonio Alvarez Jimenez <aaj@taric.es>
2021-04-08 08:21:07 +0200

    fix bugs

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.post import Post, PostsList
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:galway@localhost/data')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:galway@localhost/data'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
