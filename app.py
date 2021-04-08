9173c0b2c25235226cbf3a31a314ed2780125487
Antonio Alvarez Jimenez <aaj@taric.es>
2021-04-08 10:10:44 +0200

    fix bugs

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 api.add_resource(Post, '/post/<string:post_id>')
 api.add_resource(PostsList, '/posts')
